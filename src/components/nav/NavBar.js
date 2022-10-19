8d04030dc6f550898b7a19cb3b98d2576b2b30de
Justin Dority <justindority@gmail.com>
2022-10-19 13:52:42 -0500

    inital users work commit

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -33,7 +33,10 @@ export const NavBar = ({ token, setToken }) => {
           {
             token
               ?
+              <>
               <Link to="/" className="navbar-item">Posts</Link>
+              <Link to="/users" className="navbar-item">Users</Link>
+              </>
               :
               ""
           }
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
+import { UserList } from "../components/users/UserList"
 import { Authorized } from "./Authorized"
 
 export const ApplicationViews = ({ token, setToken }) => {
@@ -8,6 +9,7 @@ export const ApplicationViews = ({ token, setToken }) => {
     <Routes>
       <Route path="/login" element={<Login setToken={setToken} />}  />
       <Route path="/register" element={<Register setToken={setToken} />}  />
+      <Route path="/users" element={<UserList setToken={setToken} />}  />
       <Route element={<Authorized token={token} />}>
         {/* Add Routes here */}
         
