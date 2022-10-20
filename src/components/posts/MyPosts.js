16bc1e9c8839fbf8563e77085563402f07ffdcd5
Nora Szeto <neszeto@gmail.com>
2022-10-20 11:10:43 -0500

    working on myposts

--- src/components/posts/MyPosts.js
+++ src/components/posts/MyPosts.js
@@ -21,29 +21,29 @@ export const MyPosts = () => {
 
     
     return <section>
-    <h2>My Posts</h2>
-    <div className = "postsSection">
-        {
-            allPosts.map(
-                (post) => {
-                    if (userObject === post.user_id) {
-                        return <li className="postBox">
-                        <img className="postPic" src={post.image_url} width="600px" alt=""></img>
-                        <Link className="postName" to={`/posts/${post.id}`}>{post?.title}</Link>
-                        <div className="postInfo">
-                            <p>Author: {post.author}</p>
-                            <p>Category: {post.category}</p>
-                        </div>
-                    </li>
+        <h2>My Posts</h2>
+        <div className = "postsSection">
+            {
+                allPosts.map(
+                    (post) => {
+                        if (userObject === post.user_id) {
+                            return <li className="postBox">
+                                <img className="postPic" src={post.image_url} width="600px" alt=""></img>
+                                <Link className="postName" to={`/posts/${post.id}`}>{post?.title}</Link>
+                                <div className="postInfo">
+                                    <p>Author: {post.author}</p>
+                                    <p>Category: {post.category}</p>
+                                </div>
+                            </li>
 
+                        }
+                        else {
+                            <div>You have not created any posts yet!</div>
+                        }
                     }
-                    else {
-                        <div>You have not created any posts yet!</div>
-                    }
-                }
-            )
-        }
-    </div>
+                )
+            }
+        </div>
     </section>
     
     
