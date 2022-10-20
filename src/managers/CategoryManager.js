5125e4303348db5551ec6fa35230c067addf977a
Justin Dority <justindority@gmail.com>
2022-10-20 15:42:14 -0500

    posting new categories works

--- src/managers/CategoryManager.js
+++ src/managers/CategoryManager.js
@@ -2,3 +2,13 @@ export const getCategories = () => {
     return fetch(`http://localhost:8088/categories?_sortBy=label`)
         .then(res => res.json())
 }
+
+export const createCategory = (cat) => {
+    return fetch("http://localhost:8088/categories", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(cat)
+    })
+}
