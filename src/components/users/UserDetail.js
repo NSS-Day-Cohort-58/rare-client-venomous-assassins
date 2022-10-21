c9eaecd3d7a4ee8c4d2a802cfe9c3e663c7ba09f
Justin Dority <justindority@gmail.com>
2022-10-21 14:34:49 -0500

    fixed create date field

--- src/components/users/UserDetail.js
+++ src/components/users/UserDetail.js
@@ -18,7 +18,7 @@ export const UserDetail = () => {
         <div className="user-photo"><img src={user?.profile_image_url} alt="image"></img></div>
         <div className="user-name">Name: {user?.first_name} {user?.last_name}</div>
         <div className="user-username">Username: {user?.username} </div>
-        <div className="user-date">Creation Date: {user?.creation_date} </div>
+        <div className="user-date">Creation Date: {user?.created_on} </div>
         <div className="user-bio">Bio: {user?.bio}</div>
     </div>
 }
