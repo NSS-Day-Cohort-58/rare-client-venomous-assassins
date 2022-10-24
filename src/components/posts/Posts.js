9332c2028ed4e060f950607535392bab1406bf6f
Lorin Jones <Lorin.Jones@gmail.com>
2022-10-24 11:30:38 -0400

    dropdown mostly working, but content not appearing on page load

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,8 +1,10 @@
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 import { useState } from "react"
 import { getPosts } from "../../managers/PostManager"
 import { getCategories } from "../../managers/CategoryManager"
+
+
 
 
 export const Posts = () => {
@@ -10,10 +12,15 @@ export const Posts = () => {
     const [allPosts, setAllPosts] = useState([])
     const [allCategories, setCategories] = useState([])
     const [selectedPosts, setSelectedPosts] = useState([])
-    const [selectedCategory, setSelectedCategory] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState()
+    
 
     useEffect(() => {
         getCategories().then(categoryData => setCategories(categoryData))
+    }, [])
+
+    useEffect(() => {
+        setSelectedCategory(0)
     }, [])
 
     useEffect(
@@ -24,40 +31,32 @@ export const Posts = () => {
                 })
         },
         [])
+
+    
     //state to hold all posts
     //state to hold filtered posts
-    useEffect(
-        () => {
-            selectedPosts
-            ?
-            setAllPosts(selectedPosts)
-            :
-            setAllPosts(allPosts)
-        },
-    [selectedPosts])
-
+    
     useEffect(
         () => {
             let selectedPostsArray = []
-            allPosts.map(
-                (post) => {
-                    if (selectedCategory === post.category_id) {
-                        selectedPostsArray.push(post)
+            if (selectedCategory !== 0) {
+                allPosts.map(
+                    (post) => {
+                        if (selectedCategory === post.category_id) {
+                            selectedPostsArray.push(post)
+                        }
                     }
-                }
-            )
-            setSelectedPosts(selectedPostsArray)
-        },
-    [selectedCategory])
+                    )
+                    setSelectedPosts(selectedPostsArray)
+            } else {
+                setSelectedPosts(allPosts)
+            }},
+        [selectedCategory])
+            
     
-    document.addEventListener(
-        "change",
-        (changeEvent) => {
-            if (changeEvent.target.id === "category") {
-                setSelectedCategory(parseInt(changeEvent.target.value))
-            }
-        }
-    )
+    let handleCategoryChange = (e) => {
+        setSelectedCategory(parseInt(e.target.value))
+    }        
 
 
 //separate module for dropdown function
@@ -69,27 +68,23 @@ export const Posts = () => {
 
     return <section>
         <h2>All Posts</h2>
-        <div className="searchByCategory">
-            {   <>
-                    <h2>Search By Category</h2>
-                    <select id="category">
-                        <option value="0">Select Category</option>
-                        {
-                            allCategories.map(
-                                (category) => {
-                                    return <option selected value={category.id}>{category.label}</option>
-                                }
-                            )
-                        }
-                    </select>
-            
-                </>
-            }
+        <div className="category">
+            {/* Displaying the value of fruit */}
+            {selectedCategory}
+            <br />
+
+            <select onChange={handleCategoryChange}> 
+                <option value={0}>Select a Category</option>
+                        {/* Mapping through each fruit object in our fruits array
+                    and returning an option element with the appropriate attributes / values.
+                    */}
+                {allCategories.map((category) => <option value={category.id}>{category.label}</option>)}
+            </select>
         </div>
         <div className="postsSection">
             <div>
                 {   
-                    allPosts.map((post) => {
+                    selectedPosts.map((post) => {
                         return <li className="postBox">
                             <img className="postPic" src={post.image_url} width="600px" alt=""></img>
                             <Link className="postName" to={`/posts/${post.id}`}>{post?.title}</Link>
