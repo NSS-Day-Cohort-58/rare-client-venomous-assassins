487b6987ccb83a4682f20b5d6cf47560d2f14f5a
Lorin Jones <Lorin.Jones@gmail.com>
2022-10-25 11:23:52 -0400

    changed client fetch for posts by category and user

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 import { useState } from "react"
-import { getPosts } from "../../managers/PostManager"
+import { getPosts, getPostsByCategory, getPostsByUser } from "../../managers/PostManager"
 import { getCategories } from "../../managers/CategoryManager"
 import { getUsers } from "../../managers/UserManager"
 
@@ -13,9 +13,8 @@ export const Posts = () => {
     const [allPosts, setAllPosts] = useState([])
     const [allCategories, setCategories] = useState([])
     const [selectedPosts, setSelectedPosts] = useState([])
-    const [selectedCategory, setSelectedCategory] = useState(0)
     const [allUsers, setAllUsers] = useState([])
-    const [selectedUser, setSelectedUser] = useState(0)
+    
     
 
     useEffect(() => {
@@ -30,126 +29,32 @@ export const Posts = () => {
                     setAllUsers(allUsersArray)
                 })
     }, [])
-    
-        
-//pulls in posts, users, categories, and holds them in state
-//if user is selected from dropdown, filter posts based on user id
-//if category is selected from dropdown, filter posts based on category id    
-    //state to hold all posts
-    //state to hold filtered posts
-    
-    useEffect(
-        () => {
-            let selectedPostsArray = []
-            if (selectedCategory !== 0) {
-                
-                allPosts.map(
-                    (post) => {
-                        if (selectedCategory === post.category_id) {
-                            selectedPostsArray.push(post)
-                        }
-                    }
-                    )
-                    
-                    setSelectedPosts(selectedPostsArray)
-            } else if (selectedUser === 0){
-                
-                setSelectedPosts(allPosts)
-            }
-            setSelectedUser(0)
-        },
-        [selectedCategory])
-
-    useEffect(
-        () => {
-            
-            let selectedPostsArray = []
-            if (selectedUser !== 0) {
-                
-                allPosts.map(
-                    (post) => {
-                        if (selectedUser === post.user_id) {
-                            selectedPostsArray.push(post)
-                        }
-                    }
-                    )
-                    
-                    setSelectedPosts(selectedPostsArray)
-            } else if (selectedCategory === 0){
-                setSelectedPosts(allPosts)
-            }
-            setSelectedCategory(0)
-        },
-        [selectedUser])
-
-    // useEffect(
-    //     () => {
-    //         let selectedPostsArray = []
-    //         if (selectedCategory !== 0) {
-    //             allPosts.map(
-    //                 (post) => {
-    //                     if (selectedCategory === post.category_id) {
-    //                         selectedPostsArray.push(post)
-    //                     }
-    //                 }
-    //                 )
-    //                 setSelectedPosts(selectedPostsArray)
-    //         } else if (selectedUser !== 0) {
-    //             allPosts.map(
-    //                 (post) => {
-    //                     if (selectedUser === post.user_id) {
-    //                         selectedPostsArray.push(post)
-    //                     }
-    //                 }
-    //                 )
-    //                 setSelectedPosts(selectedPostsArray)
-    //         } else {
-    //                 setSelectedPosts(allPosts)
-                    
-    //         }},
-    //     [allPosts])
-            
+   
     
     let handleCategoryChange = (e) => {
-        setSelectedCategory(parseInt(e.target.value))
+        getPostsByCategory(parseInt(e.target.value))
+            .then(postsByCategoryArray => {
+                setSelectedPosts(postsByCategoryArray)
+            })
     }        
     let handleAuthorChange = (a) => {
-        setSelectedUser(parseInt(a.target.value))
+        getPostsByUser(parseInt(a.target.value))
+            .then(postsByUserArray => {
+                setSelectedPosts(postsByUserArray)
+            })
     }
-
-
-//separate module for dropdown function
-//export const Categories
-//let html = <select id="category">
-//html += <option value ="0">Search by Category</option>
-//map through categories
-//<option selected value="${category.id}">${category.name}</option>
 
     return <section>
         <h2>All Posts</h2>
         <div className="category">
-            {/* Displaying the value of fruit */}
-            
-            <br />
-
-            <select value ={selectedCategory} onChange={handleCategoryChange}> 
+            <select onChange={handleCategoryChange}> 
                 <option value={0}>Search by Category</option>
-                        {/* Mapping through each fruit object in our fruits array
-                    and returning an option element with the appropriate attributes / values.
-                    */}
                 {allCategories.map((category) => <option value={category.id}>{category.label}</option>)}
             </select>
         </div>
         <div className="author">
-            {/* Displaying the value of fruit */}
-            
-            <br />
-
-            <select value={selectedUser} onChange={handleAuthorChange}> 
+            <select onChange={handleAuthorChange}> 
                 <option value={0}>Search by Author</option>
-                        {/* Mapping through each fruit object in our fruits array
-                    and returning an option element with the appropriate attributes / values.
-                    */}
                 {allUsers.map((user) => <option value={user.id}>{user.username}</option>)}
             </select>
         </div>
--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -5,3 +5,11 @@ export const getPosts = () => {
         .then(response => response.json())
 }
 
+export const getPostsByCategory = (categoryId) => {
+    return fetch(`http://localhost:8088/posts?category_id=${categoryId}`)
+        .then(response => response.json())
+}
+
+export const getPostsByUser = (userId) => {
+    return fetch(`http://localhost:8088/posts?user_id=${userId}`)
+}
