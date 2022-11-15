69027db6c78dfb2f0f04ddb88a8d51a069fbc8fd
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-11-15 15:15:50 -0600

    Fixing bugs

--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -1,7 +1,7 @@
 
 
 export const getPosts = () => { 
-    return fetch(`http://localhost:8088/posts`)
+    return fetch(`http://localhost:8000/posts`)
         .then(response => response.json())
 }
 
