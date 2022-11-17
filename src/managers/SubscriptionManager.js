a7ca50cce41363587a84a1d88dfbecb892cc0e9d
James Scott <jsa.codes@gmail.com>
2022-11-17 11:23:31 -0600

    Corrected spelling of function name Subscription in Subscription Manager

--- src/managers/SubscriptionManager.js
+++ src/managers/SubscriptionManager.js
@@ -1,7 +1,7 @@
 export const getSubscriptions = () => {
     return fetch("http://localhost:8088/subscriptions")
         .then(res => res.json())
-  }
+}
 
 export const createSubscription = (sub) => {
     return fetch("http://localhost:8088/subscriptions", {
@@ -13,7 +13,7 @@ export const createSubscription = (sub) => {
     })
 }
 
-export const deleteSubscripton = (event) => {
+export const deleteSubscription = (event) => {
     return fetch(`http://localhost:8088/subscriptions/${parseInt(event.target.id)}`, {
         method: "DELETE"
     })
